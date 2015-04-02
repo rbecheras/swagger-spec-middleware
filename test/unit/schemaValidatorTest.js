277078161866cf789bcc318fe6adbbbdc08114ad
Piotr Kosmowski <kospiotr@gmail.com>
2015-04-03 00:05:45 +0200

    Add tests for validating official specifications

--- test/unit/schemaValidatorTest.js
+++ test/unit/schemaValidatorTest.js
@@ -4,35 +4,34 @@ define(function (require) {
     var expect = require('intern/chai!expect');
     var fs = require('intern/dojo/node!fs');
 
+    function validateSpecFile(filePath) {
+        var spec = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        schemaValidator.validateAgainstSwaggerSchema(spec);
+    }
+
     suite({
         'should official petstore pass validation': function () {
-            var spec = JSON.parse(fs.readFileSync('test/resources/official/petstore.json', 'utf8'));
-            schemaValidator.validateAgainstSwaggerSchema(spec);
+            validateSpecFile('test/resources/official/petstore.json');
         },
         'should official petstore-expanded pass validation': function () {
-            var spec = JSON.parse(fs.readFileSync('test/resources/official/petstore-expanded.json', 'utf8'));
-            schemaValidator.validateAgainstSwaggerSchema(spec);
+            validateSpecFile('test/resources/official/petstore-expanded.json');
         },
         'should official petstore-minimal pass validation': function () {
-            var spec = JSON.parse(fs.readFileSync('test/resources/official/petstore-minimal.json', 'utf8'));
-            schemaValidator.validateAgainstSwaggerSchema(spec);
+            validateSpecFile('test/resources/official/petstore-minimal.json');
         },
         'should official petstore-simple pass validation': function () {
-            var spec = JSON.parse(fs.readFileSync('test/resources/official/petstore-simple.json', 'utf8'));
-            schemaValidator.validateAgainstSwaggerSchema(spec);
+            validateSpecFile('test/resources/official/petstore-simple.json');
         },
         'should official petstore-with-external-docs pass validation': function () {
-            var spec = JSON.parse(fs.readFileSync('test/resources/official/petstore-with-external-docs.json', 'utf8'));
-            schemaValidator.validateAgainstSwaggerSchema(spec);
+            validateSpecFile('test/resources/official/petstore-with-external-docs.json');
         },
         'should toystore pass schema validation': function () {
-            var spec = JSON.parse(fs.readFileSync('test/resources/toyStore.json', 'utf8'));
-            schemaValidator.validateAgainstSwaggerSchema(spec);
+            validateSpecFile('test/resources/toyStore.json');
         },
         'should invalidSchema not pass validation': function () {
-            var spec = JSON.parse(fs.readFileSync('test/resources/invalidSchemaSpec.json', 'utf8'));
+            validateSpecFile('test/resources/invalidSchemaSpec.json');
             try {
-                schemaValidator.validateAgainstSwaggerSchema(spec);
+                validateSpecFile('test/resources/invalidSchemaSpec.json');
                 expect().fail('invalid shema shoul not pass validation');
             } catch (e) {
                 expect(e).not.to.be.null;
